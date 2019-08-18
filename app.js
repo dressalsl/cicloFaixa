310560949ecf36b0d96591a988381c9dbefe7c4d
[Andressa Santana] <[dressalsl97@gmail.com]>
2019-08-18 20:06:24 -0300

    sequelize e outros add

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+var Sequelize = require('sequelize'); 
+
+var connection = new Sequelize('crud','root',''); 
+
+var conexao = connection.authenticate()
+
+.then(function(){
+    console.log('conexão com sevidor!');
+})
+
+.catch(function(err){
+ 
+    
+    console.log('O servidor não está conectado com o mysql!');
+})
+  
+
+ 
+.done(); 
+
+/*
 var http = require('http'); 
 http.createServer().listen (8081); 
 console.log("o servidor esta rodando!")
+
+*/
